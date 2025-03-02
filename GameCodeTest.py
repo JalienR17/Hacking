9e737ef900d0638bb2c279f27f5b5d67726b7bd4
Jose Rojas <jalienr17@icloud.com>
2025-03-01 23:37:25 -0500

    added GameCodeTest.py

---
+++ GameCodeTest.py
@@ -0,0 +1,12 @@
+difficulty = input("choose '1' for 'easy', '2' for 'normal', '3' for 'hard', or '4' for 'impossible' :  ")
+
+if difficulty == "1":
+    print("easy difficulty chosen.")
+elif difficulty == "2":
+    print("normal difficulty chosen.")
+elif difficulty == "3":
+    print("hard difficulty chosen.")
+elif difficulty == "4":
+    print("impossible difficulty chosen.")
+else:
+    print("please choose a valid difficulty.")
